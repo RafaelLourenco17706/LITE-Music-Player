fd187b73c78f1b3a3f69669c71e374a1fd19de7a
RafaelLourenco17706 <168860760+RafaelLourenco17706@users.noreply.github.com>
2024-06-04 21:47:17 +0100

    volume slider, new button styles

--- mediaplayer/FormMain.Designer.cs
+++ mediaplayer/FormMain.Designer.cs
@@ -29,33 +29,32 @@ namespace mediaplayer
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnPlay = new System.Windows.Forms.Button();
             this.btnPause = new System.Windows.Forms.Button();
-            this.btnStop = new System.Windows.Forms.Button();
-            this.btnPrevious = new System.Windows.Forms.Button();
-            this.btnNext = new System.Windows.Forms.Button();
-            this.btnPlaylist = new System.Windows.Forms.Button();
-            this.btnShuffle = new System.Windows.Forms.Button();
             this.lblCurrentTime = new System.Windows.Forms.Label();
             this.lblTotalTime = new System.Windows.Forms.Label();
-            this.slider = new mediaplayer.Slider();
-            ((System.ComponentModel.ISupportInitialize)(this.slider)).BeginInit();
+            this.btnPlaylist = new FontAwesome.Sharp.IconPictureBox();
+            this.btnShuffle = new FontAwesome.Sharp.IconPictureBox();
+            this.btnStop = new FontAwesome.Sharp.IconPictureBox();
+            this.btnPrevious = new FontAwesome.Sharp.IconPictureBox();
+            this.btnPlay = new FontAwesome.Sharp.IconPictureBox();
+            this.btnNext = new FontAwesome.Sharp.IconPictureBox();
+            this.iconVolume = new FontAwesome.Sharp.IconPictureBox();
+            this.sliderVolume = new mediaplayer.Slider();
+            this.sliderPlayback = new mediaplayer.Slider();
+            ((System.ComponentModel.ISupportInitialize)(this.btnPlaylist)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnShuffle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnStop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnPrevious)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnPlay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnNext)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconVolume)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sliderVolume)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sliderPlayback)).BeginInit();
             this.SuspendLayout();
-            // 
-            // btnPlay
-            // 
-            this.btnPlay.Location = new System.Drawing.Point(350, 475);
-            this.btnPlay.Name = "btnPlay";
-            this.btnPlay.Size = new System.Drawing.Size(100, 50);
-            this.btnPlay.TabIndex = 0;
-            this.btnPlay.TabStop = false;
-            this.btnPlay.Text = "play";
-            this.btnPlay.UseVisualStyleBackColor = true;
-            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
             // 
             // btnPause
             // 
-            this.btnPause.Location = new System.Drawing.Point(455, 475);
+            this.btnPause.Location = new System.Drawing.Point(670, 12);
             this.btnPause.Name = "btnPause";
             this.btnPause.Size = new System.Drawing.Size(100, 50);
             this.btnPause.TabIndex = 1;
@@ -64,70 +63,11 @@ namespace mediaplayer
             this.btnPause.UseVisualStyleBackColor = true;
             this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
             // 
-            // btnStop
-            // 
-            this.btnStop.Location = new System.Drawing.Point(245, 475);
-            this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(100, 50);
-            this.btnStop.TabIndex = 2;
-            this.btnStop.TabStop = false;
-            this.btnStop.Text = "stop";
-            this.btnStop.UseVisualStyleBackColor = true;
-            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
-            // 
-            // btnPrevious
-            // 
-            this.btnPrevious.Location = new System.Drawing.Point(21, 475);
-            this.btnPrevious.Name = "btnPrevious";
-            this.btnPrevious.Size = new System.Drawing.Size(50, 50);
-            this.btnPrevious.TabIndex = 3;
-            this.btnPrevious.TabStop = false;
-            this.btnPrevious.Text = "<";
-            this.btnPrevious.UseVisualStyleBackColor = true;
-            this.btnPrevious.Visible = false;
-            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
-            // 
-            // btnNext
-            // 
-            this.btnNext.Location = new System.Drawing.Point(183, 475);
-            this.btnNext.Name = "btnNext";
-            this.btnNext.Size = new System.Drawing.Size(50, 50);
-            this.btnNext.TabIndex = 4;
-            this.btnNext.TabStop = false;
-            this.btnNext.Text = ">";
-            this.btnNext.UseVisualStyleBackColor = true;
-            this.btnNext.Visible = false;
-            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
-            // 
-            // btnPlaylist
-            // 
-            this.btnPlaylist.Location = new System.Drawing.Point(77, 475);
-            this.btnPlaylist.Name = "btnPlaylist";
-            this.btnPlaylist.Size = new System.Drawing.Size(100, 50);
-            this.btnPlaylist.TabIndex = 5;
-            this.btnPlaylist.TabStop = false;
-            this.btnPlaylist.Text = "playlist";
-            this.btnPlaylist.UseVisualStyleBackColor = true;
-            this.btnPlaylist.Visible = false;
-            this.btnPlaylist.Click += new System.EventHandler(this.btnPlaylist_Click);
-            // 
-            // btnShuffle
-            // 
-            this.btnShuffle.Location = new System.Drawing.Point(21, 400);
-            this.btnShuffle.Name = "btnShuffle";
-            this.btnShuffle.Size = new System.Drawing.Size(100, 50);
-            this.btnShuffle.TabIndex = 6;
-            this.btnShuffle.TabStop = false;
-            this.btnShuffle.Text = "shuffle";
-            this.btnShuffle.UseVisualStyleBackColor = true;
-            this.btnShuffle.Visible = false;
-            this.btnShuffle.Click += new System.EventHandler(this.btnShuffle_Click);
-            // 
             // lblCurrentTime
             // 
             this.lblCurrentTime.AutoSize = true;
             this.lblCurrentTime.ForeColor = System.Drawing.SystemColors.Control;
-            this.lblCurrentTime.Location = new System.Drawing.Point(36, 359);
+            this.lblCurrentTime.Location = new System.Drawing.Point(30, 435);
             this.lblCurrentTime.Name = "lblCurrentTime";
             this.lblCurrentTime.Size = new System.Drawing.Size(64, 17);
             this.lblCurrentTime.TabIndex = 8;
@@ -137,25 +77,143 @@ namespace mediaplayer
             // 
             this.lblTotalTime.AutoSize = true;
             this.lblTotalTime.ForeColor = System.Drawing.SystemColors.Control;
-            this.lblTotalTime.Location = new System.Drawing.Point(690, 360);
+            this.lblTotalTime.Location = new System.Drawing.Point(690, 435);
             this.lblTotalTime.Name = "lblTotalTime";
             this.lblTotalTime.Size = new System.Drawing.Size(64, 17);
             this.lblTotalTime.TabIndex = 9;
             this.lblTotalTime.Text = "00:00:00";
             // 
-            // slider
+            // btnPlaylist
             // 
-            this.slider.Enabled = false;
-            this.slider.LargeChange = 0;
-            this.slider.Location = new System.Drawing.Point(21, 300);
-            this.slider.Maximum = 100;
-            this.slider.Name = "slider";
-            this.slider.Size = new System.Drawing.Size(733, 56);
-            this.slider.SmallChange = 0;
-            this.slider.TabIndex = 10;
-            this.slider.TickFrequency = 0;
-            this.slider.MouseDown += new System.Windows.Forms.MouseEventHandler(this.slider_MouseDown);
-            this.slider.MouseUp += new System.Windows.Forms.MouseEventHandler(this.slider_MouseUp);
+            this.btnPlaylist.BackColor = System.Drawing.Color.Transparent;
+            this.btnPlaylist.ForeColor = System.Drawing.Color.Gray;
+            this.btnPlaylist.IconChar = FontAwesome.Sharp.IconChar.FolderOpen;
+            this.btnPlaylist.IconColor = System.Drawing.Color.Gray;
+            this.btnPlaylist.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnPlaylist.IconSize = 50;
+            this.btnPlaylist.Location = new System.Drawing.Point(498, 295);
+            this.btnPlaylist.Margin = new System.Windows.Forms.Padding(0);
+            this.btnPlaylist.Name = "btnPlaylist";
+            this.btnPlaylist.Size = new System.Drawing.Size(50, 50);
+            this.btnPlaylist.TabIndex = 23;
+            this.btnPlaylist.TabStop = false;
+            // 
+            // btnShuffle
+            // 
+            this.btnShuffle.BackColor = System.Drawing.Color.Transparent;
+            this.btnShuffle.ForeColor = System.Drawing.Color.Gray;
+            this.btnShuffle.IconChar = FontAwesome.Sharp.IconChar.Random;
+            this.btnShuffle.IconColor = System.Drawing.Color.Gray;
+            this.btnShuffle.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnShuffle.IconSize = 40;
+            this.btnShuffle.Location = new System.Drawing.Point(567, 305);
+            this.btnShuffle.Margin = new System.Windows.Forms.Padding(0);
+            this.btnShuffle.Name = "btnShuffle";
+            this.btnShuffle.Size = new System.Drawing.Size(40, 40);
+            this.btnShuffle.TabIndex = 22;
+            this.btnShuffle.TabStop = false;
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Transparent;
+            this.btnStop.ForeColor = System.Drawing.Color.Gray;
+            this.btnStop.IconChar = FontAwesome.Sharp.IconChar.Stop;
+            this.btnStop.IconColor = System.Drawing.Color.Gray;
+            this.btnStop.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnStop.IconSize = 50;
+            this.btnStop.Location = new System.Drawing.Point(242, 480);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(0);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(50, 50);
+            this.btnStop.TabIndex = 21;
+            this.btnStop.TabStop = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.BackColor = System.Drawing.Color.Transparent;
+            this.btnPrevious.ForeColor = System.Drawing.Color.Gray;
+            this.btnPrevious.IconChar = FontAwesome.Sharp.IconChar.BackwardStep;
+            this.btnPrevious.IconColor = System.Drawing.Color.Gray;
+            this.btnPrevious.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnPrevious.IconSize = 50;
+            this.btnPrevious.Location = new System.Drawing.Point(567, 234);
+            this.btnPrevious.Margin = new System.Windows.Forms.Padding(0);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(50, 50);
+            this.btnPrevious.TabIndex = 20;
+            this.btnPrevious.TabStop = false;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.Color.Transparent;
+            this.btnPlay.IconChar = FontAwesome.Sharp.IconChar.Play;
+            this.btnPlay.IconColor = System.Drawing.Color.White;
+            this.btnPlay.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnPlay.IconSize = 50;
+            this.btnPlay.Location = new System.Drawing.Point(369, 480);
+            this.btnPlay.Margin = new System.Windows.Forms.Padding(0);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(50, 50);
+            this.btnPlay.TabIndex = 18;
+            this.btnPlay.TabStop = false;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.BackColor = System.Drawing.Color.Transparent;
+            this.btnNext.ForeColor = System.Drawing.Color.Gray;
+            this.btnNext.IconChar = FontAwesome.Sharp.IconChar.ForwardStep;
+            this.btnNext.IconColor = System.Drawing.Color.Gray;
+            this.btnNext.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnNext.IconSize = 50;
+            this.btnNext.Location = new System.Drawing.Point(670, 234);
+            this.btnNext.Margin = new System.Windows.Forms.Padding(0);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(50, 50);
+            this.btnNext.TabIndex = 19;
+            this.btnNext.TabStop = false;
+            // 
+            // iconVolume
+            // 
+            this.iconVolume.BackColor = System.Drawing.Color.Transparent;
+            this.iconVolume.IconChar = FontAwesome.Sharp.IconChar.VolumeHigh;
+            this.iconVolume.IconColor = System.Drawing.Color.White;
+            this.iconVolume.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconVolume.IconSize = 40;
+            this.iconVolume.Location = new System.Drawing.Point(603, 485);
+            this.iconVolume.Margin = new System.Windows.Forms.Padding(0);
+            this.iconVolume.Name = "iconVolume";
+            this.iconVolume.Size = new System.Drawing.Size(40, 40);
+            this.iconVolume.TabIndex = 17;
+            this.iconVolume.TabStop = false;
+            // 
+            // sliderVolume
+            // 
+            this.sliderVolume.LargeChange = 0;
+            this.sliderVolume.Location = new System.Drawing.Point(652, 474);
+            this.sliderVolume.Maximum = 100;
+            this.sliderVolume.Name = "sliderVolume";
+            this.sliderVolume.Size = new System.Drawing.Size(102, 56);
+            this.sliderVolume.SmallChange = 0;
+            this.sliderVolume.TabIndex = 24;
+            this.sliderVolume.TickFrequency = 0;
+            this.sliderVolume.Value = 100;
+            this.sliderVolume.ValueChanged += new System.EventHandler(this.sliderVolume_ValueChanged);
+            // 
+            // sliderPlayback
+            // 
+            this.sliderPlayback.Enabled = false;
+            this.sliderPlayback.LargeChange = 0;
+            this.sliderPlayback.Location = new System.Drawing.Point(21, 383);
+            this.sliderPlayback.Maximum = 100;
+            this.sliderPlayback.Name = "sliderPlayback";
+            this.sliderPlayback.Size = new System.Drawing.Size(733, 56);
+            this.sliderPlayback.SmallChange = 0;
+            this.sliderPlayback.TabIndex = 10;
+            this.sliderPlayback.TickFrequency = 0;
+            this.sliderPlayback.MouseDown += new System.Windows.Forms.MouseEventHandler(this.slider_MouseDown);
+            this.sliderPlayback.MouseUp += new System.Windows.Forms.MouseEventHandler(this.slider_MouseUp);
             // 
             // FormMain
             // 
@@ -163,40 +221,51 @@ namespace mediaplayer
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
             this.ClientSize = new System.Drawing.Size(782, 553);
-            this.Controls.Add(this.slider);
+            this.Controls.Add(this.sliderVolume);
+            this.Controls.Add(this.btnPlaylist);
+            this.Controls.Add(this.btnShuffle);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.iconVolume);
+            this.Controls.Add(this.sliderPlayback);
             this.Controls.Add(this.lblTotalTime);
             this.Controls.Add(this.lblCurrentTime);
-            this.Controls.Add(this.btnShuffle);
-            this.Controls.Add(this.btnPlaylist);
-            this.Controls.Add(this.btnNext);
-            this.Controls.Add(this.btnPrevious);
-            this.Controls.Add(this.btnStop);
             this.Controls.Add(this.btnPause);
-            this.Controls.Add(this.btnPlay);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "FormMain";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Media Player";
-            ((System.ComponentModel.ISupportInitialize)(this.slider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnPlaylist)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnShuffle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnStop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnPrevious)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnPlay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnNext)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconVolume)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sliderVolume)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sliderPlayback)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnPlay;
         private System.Windows.Forms.Button btnPause;
-        private System.Windows.Forms.Button btnStop;
-        private System.Windows.Forms.Button btnPrevious;
-        private System.Windows.Forms.Button btnNext;
-        private System.Windows.Forms.Button btnPlaylist;
-        private System.Windows.Forms.Button btnShuffle;
         private System.Windows.Forms.Label lblCurrentTime;
         private System.Windows.Forms.Label lblTotalTime;
-        private Slider slider;
+        private Slider sliderPlayback;
+        private FontAwesome.Sharp.IconPictureBox btnPlaylist;
+        private FontAwesome.Sharp.IconPictureBox btnShuffle;
+        private FontAwesome.Sharp.IconPictureBox btnStop;
+        private FontAwesome.Sharp.IconPictureBox btnPrevious;
+        private FontAwesome.Sharp.IconPictureBox btnPlay;
+        private FontAwesome.Sharp.IconPictureBox btnNext;
+        private FontAwesome.Sharp.IconPictureBox iconVolume;
+        private Slider sliderVolume;
     }
 }
 
--- mediaplayer/FormMain.cs
+++ mediaplayer/FormMain.cs
@@ -17,7 +17,7 @@ namespace mediaplayer
     {
         public static WaveOutEvent waveOutDevice;
         public static AudioFileReader audioFileReader;
-        private System.Timers.Timer timer;
+        private readonly System.Timers.Timer timer;
 
         public static bool playlistIsActive;
         public static bool userStopped;
@@ -30,10 +30,11 @@ namespace mediaplayer
 
             timer = new System.Timers.Timer();
             timer.Interval = 100;
-            timer.Elapsed += UpdateSlider;
+            timer.Elapsed += UpdateSliderPlayback;
             timer.Start();
         }
 
+        
         public void FormUpdate()
         {
             if (playlistIsActive)
@@ -50,14 +51,25 @@ namespace mediaplayer
             }
         }
 
-        #region slider
+        #region Sliders
 
-        private void UpdateSlider(object sender, ElapsedEventArgs e)
+        private void sliderVolume_ValueChanged(object sender, EventArgs e)
+        {
+            if (waveOutDevice != null)
+                waveOutDevice.Volume = sliderVolume.Value / 100f;
+
+            if (sliderVolume.Value == 0)
+                iconVolume.IconChar = FontAwesome.Sharp.IconChar.VolumeMute;
+            else
+                iconVolume.IconChar = FontAwesome.Sharp.IconChar.VolumeHigh;
+        }
+
+        private void UpdateSliderPlayback(object sender, ElapsedEventArgs e)
         {
             if (waveOutDevice?.PlaybackState == PlaybackState.Playing)
             {
                 if (isDragging == false)
-                    Invoke(new Action(() => slider.Value = (int)audioFileReader.CurrentTime.TotalSeconds));
+                    try { Invoke(new Action(() => sliderPlayback.Value = (int)audioFileReader.CurrentTime.TotalSeconds)); } catch { }
             }
 
             if (waveOutDevice?.PlaybackState == PlaybackState.Playing || waveOutDevice?.PlaybackState == PlaybackState.Paused)
@@ -65,7 +77,7 @@ namespace mediaplayer
                 int seconds = ((int)audioFileReader.CurrentTime.TotalSeconds);
                 TimeSpan time = TimeSpan.FromSeconds(seconds);
 
-                Invoke(new Action(() => lblCurrentTime.Text = time.ToString(@"hh\:mm\:ss")));
+                try { Invoke(new Action(() => lblCurrentTime.Text = time.ToString(@"hh\:mm\:ss"))); } catch { }
             }
         }
 
@@ -79,7 +91,7 @@ namespace mediaplayer
             isDragging = false;
             if (audioFileReader != null)
             {
-                audioFileReader.CurrentTime = TimeSpan.FromSeconds(slider.Value);
+                audioFileReader.CurrentTime = TimeSpan.FromSeconds(sliderPlayback.Value);
             }
         }
 
@@ -87,47 +99,58 @@ namespace mediaplayer
 
         #region Buttons
 
-        private void btnPlay_Click(object sender, EventArgs e)
+        private async void btnPlay_Click(object sender, EventArgs e)
         {
             if (waveOutDevice == null)
             {
-                OpenFileDialog openFileDialog = new OpenFileDialog();
-                openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
-                openFileDialog.Filter = "mp3 files (*.mp3)|*.mp3";
-                openFileDialog.ShowDialog();
-
-                if (openFileDialog.FileName != "")
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
                 {
-                    waveOutDevice = new WaveOutEvent();
-                    audioFileReader = new AudioFileReader(openFileDialog.FileName);
-                    waveOutDevice.Init(audioFileReader);
+                    openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+                    openFileDialog.Filter = "Audio Files (*.mp3;*.wav;*.aac;*.flac;*.m4a;*.aiff;*.wma) | *.mp3;*.wav;*.aac;*.flac;*.m4a;*.aiff;*.wma";
+                    openFileDialog.ShowDialog();
+
+                    if (openFileDialog.FileName != "")
+                    {
+                        waveOutDevice = new WaveOutEvent();
+                        audioFileReader = new AudioFileReader(openFileDialog.FileName);
+                        waveOutDevice.Init(audioFileReader);
+                        waveOutDevice.Volume = sliderVolume.Value / 100f;
+                        waveOutDevice.Play();
+                        waveOutDevice.PlaybackStopped += btnStop_Click;
+
+                        btnPlay.IconChar = FontAwesome.Sharp.IconChar.Pause;
+
+                        int seconds = ((int)audioFileReader.TotalTime.TotalSeconds);
+                        TimeSpan time = TimeSpan.FromSeconds(seconds);
+                        lblTotalTime.Text = time.ToString(@"hh\:mm\:ss");
+
+                        sliderPlayback.Maximum = (int)audioFileReader.TotalTime.TotalSeconds;
+
+                        // Prevent user from accidentally moving the slider when selecting file
+                        await Task.Delay(100);
+
+                        sliderPlayback.Enabled = true;  
+                    }
+                }  
+            }
+            else
+            {
+                if (waveOutDevice.PlaybackState == PlaybackState.Playing)
+                {
+                    waveOutDevice.Pause();
+                    btnPlay.IconChar = FontAwesome.Sharp.IconChar.Play;
+                }
+                else if (waveOutDevice.PlaybackState == PlaybackState.Paused)
+                {
                     waveOutDevice.Play();
-                    waveOutDevice.PlaybackStopped += btnStop_Click;
-
-                    slider.Maximum = (int)audioFileReader.TotalTime.TotalSeconds;
-
-                    int seconds = ((int)audioFileReader.TotalTime.TotalSeconds);
-                    TimeSpan time = TimeSpan.FromSeconds(seconds);
-                    lblTotalTime.Text = time.ToString(@"hh\:mm\:ss");
-
-                    slider.Enabled = true;
+                    btnPlay.IconChar = FontAwesome.Sharp.IconChar.Pause;
                 }
             }
         }
 
         private void btnPause_Click(object sender, EventArgs e)
         {
-            if (waveOutDevice != null)
-            {
-                if (waveOutDevice.PlaybackState == PlaybackState.Playing)
-                {
-                    waveOutDevice.Pause();
-                }
-                else if (waveOutDevice.PlaybackState == PlaybackState.Paused)
-                {
-                    waveOutDevice.Play();
-                }
-            }
+            
         } 
 
         private void btnStop_Click(object sender, EventArgs e)
@@ -144,8 +167,10 @@ namespace mediaplayer
 
                 lblCurrentTime.Text = "00:00:00";
                 lblTotalTime.Text = "00:00:00";
-                slider.Value = 0;
-                slider.Enabled = false;
+                sliderPlayback.Value = 0;
+                sliderPlayback.Enabled = false;
+
+                btnPlay.IconChar = FontAwesome.Sharp.IconChar.Play;
             }
         }
 
