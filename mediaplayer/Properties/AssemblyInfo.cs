049438f67a3b342b67929dc6c90333bbd9fce9ed
RafaelLourenco6 <167332056+RafaelLourenco6@users.noreply.github.com>
2024-06-06 15:15:28 +0100

    assembly name

--- mediaplayer/Properties/AssemblyInfo.cs
+++ mediaplayer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("mediaplayer")]
+[assembly: AssemblyTitle("LITE Music Player")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("mediaplayer")]
+[assembly: AssemblyProduct("LITE Music Player")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
